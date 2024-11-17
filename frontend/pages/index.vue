8f016d1535759207e7f06a0cba73a71a363f1129
Liam Hjalmarsson <Liam.hjalmarsson@hotmail.com>
2024-11-17 21:01:56 +0100

    :wq

---
+++ frontend/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+  </div>
+</template>
+
+
+
+<script setup>
+  let {data} = await useFetch("http://localhost:1337/api/posts");
+  console.log(data)
+</script>
